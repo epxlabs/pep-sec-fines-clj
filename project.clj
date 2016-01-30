0cd4db83dd838815e091198b53b7733d7f53c01e
Prachetas Prabhu <prachetas.github@gmail.com>
2016-01-30 03:37:51 -0500

    Adds datomic

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [enlive "1.1.6"]])
+                 [enlive "1.1.6"]
+                 [com.datomic/datomic-pro "0.9.5344"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.postgresql/postgresql "9.3-1104-jdbc41"]]
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                 :creds :gpg}})
